00452cc13dcc61963396aa19431cb50e034e2b7c
Gaming32 <soujournme@gmail.com>
2020-12-06 12:16:59 -0600

    fix Queuesort comparison count

--- src/sorts/distribute/IndexSort.java
+++ src/sorts/distribute/IndexSort.java
@@ -7,7 +7,7 @@ import sorts.templates.Sort;
  * 
 MIT License
 
-Copyright (c) 2019 w0rthy
+Copyright (c) 2020 Gaming32
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- src/sorts/select/QueueSort.java
+++ src/sorts/select/QueueSort.java
@@ -8,16 +8,28 @@ import main.ArrayVisualizer;
 import sorts.templates.Sort;
 
 /*
- * THE WORK (AS DEFINED BELOW) IS PROVIDED UNDER THE TERMS OF THIS CREATIVE COMMONS PUBLIC LICENSE ("CCPL" OR "LICENSE").
- * THE WORK IS PROTECTED BY COPYRIGHT AND/OR OTHER APPLICABLE LAW. ANY USE OF THE WORK OTHER THAN AS AUTHORIZED UNDER THIS
- * LICENSE OR COPYRIGHT LAW IS PROHIBITED.
- * 
- * BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE TO BE BOUND BY THE TERMS OF THIS LICENSE.
- * TO THE EXTENT THIS LICENSE MAY BE CONSIDERED TO BE A CONTRACT, THE LICENSOR GRANTS YOU THE RIGHTS CONTAINED HERE IN
- * CONSIDERATION OF YOUR ACCEPTANCE OF SUCH TERMS AND CONDITIONS.
- */
+MIT License
 
-// Code refactored from the Python implementation found here: https://en.wikipedia.org/wiki/Pigeonhole_sort
+Copyright (c) 2020 Gaming32
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 final public class QueueSort extends Sort {
     public QueueSort(ArrayVisualizer arrayVisualizer) {
@@ -46,7 +58,7 @@ final public class QueueSort extends Sort {
         PriorityQueue<LinkedList<Integer>> heap = new PriorityQueue<>(sortLength / 2, new Comparator<LinkedList<Integer>>(){
             @Override
             public int compare(LinkedList<Integer> q1, LinkedList<Integer> q2) {
-                return q1.peek() - q2.peek();
+                return Reads.compareValues(q1.peek(), q2.peek());
             }
         });
 
