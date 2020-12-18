0ae390a81a78136b5219f5d3040765ea14cbeea1
thatsOven <68780017+thatsOven@users.noreply.github.com>
2020-12-18 16:45:27 +0100

    Update LAQuickSort.java

--- src/sorts/hybrid/LAQuickSort.java
+++ src/sorts/hybrid/LAQuickSort.java
@@ -2,7 +2,6 @@ package sorts.hybrid;
 
 import main.ArrayVisualizer;
 import sorts.templates.Sort;
-import sorts.select.MaxHeapSort;
 
 /*
 Logarithmic Average QuickSort 2020 Copyright (C) thatsOven
@@ -35,7 +34,6 @@ final public class LAQuickSort extends Sort {
         this.setUnreasonableLimit(0);
         this.setBogoSort(false);
     }
-    private MaxHeapSort heapSorter;
 
     public void insertionSort(int[] arr, int a, int b) {
         for (int i = a + 1; i < b; i++) {
@@ -48,6 +46,135 @@ final public class LAQuickSort extends Sort {
             }
             Writes.write(arr, j+1, key, 0.5, true, false);
         }
+    }
+
+    // Poplar HeapSort by Morwenn
+    private int hyperfloor(int n) {
+        return (int) Math.pow(2, Math.floor(Math.log(n) / Math.log(2)));
+    }
+
+    private void unchecked_insertion_sort(int[] array, int first, int last) {
+        for (int cur = first + 1; cur != last; ++cur) {
+            int sift = cur;
+            int sift_1 = cur - 1;
+            if (Reads.compareValues(array[sift], array[sift_1]) == -1) {
+                int tmp = array[sift];
+                do {
+                    Writes.write(array, sift, array[sift_1], 0.25, true, false);
+                } while (--sift != first && Reads.compareValues(tmp, array[--sift_1]) == -1);
+                Writes.write(array, sift, tmp, 0.25, true, false);
+            }
+        }
+    }
+
+    private void insertion_sort(int[] array, int first, int last) {
+        if (first == last) return;
+        this.unchecked_insertion_sort(array, first, last);
+    }
+
+    private void sift(int[] array, int first, int size) {
+        if (size < 2) return;
+
+        int root = first + (size - 1);
+        int child_root1 = root - 1;
+        int child_root2 = first + (size / 2 - 1);
+
+        while (true) {
+            int max_root = root;
+            if (Reads.compareValues(array[max_root], array[child_root1]) == -1) {
+                max_root = child_root1;
+            }
+            if (Reads.compareValues(array[max_root], array[child_root2]) == -1) {
+                max_root = child_root2;
+            }
+            if (max_root == root) return;
+
+            Writes.swap(array, root, max_root, 0.75, true, false);
+            Highlights.clearMark(2);
+            
+            size /= 2;
+            if (size < 2) return;
+
+            root = max_root;
+            child_root1 = root - 1;
+            child_root2 = max_root - (size - size / 2);
+        }
+    }
+
+    private void pop_heap_with_size(int[] array, int first, int last, int size) {
+        int poplar_size = this.hyperfloor(size + 1) - 1;
+        int last_root = last - 1;
+        int bigger = last_root;
+        int bigger_size = poplar_size;
+	    
+        int it = first;
+        while (true) {
+            int root = it + poplar_size - 1;
+            if (root == last_root) break;
+            if (Reads.compareValues(array[bigger], array[root]) == -1) {
+                bigger = root;
+                bigger_size = poplar_size;
+            }
+            it = root + 1;
+
+            size -= poplar_size;
+            poplar_size = this.hyperfloor(size + 1) - 1;
+        }
+
+        if (bigger != last_root) {
+            Writes.swap(array, bigger, last_root, 0.75, true, false);
+            Highlights.clearMark(2);
+            this.sift(array, bigger - (bigger_size - 1), bigger_size);
+        }
+    }
+
+    private void make_heap(int[] array, int first, int last) {
+        int size = last - first;
+        if (size < 2) return;
+
+        int small_poplar_size = 15;
+        if (size <= small_poplar_size) {
+            this.unchecked_insertion_sort(array, first, last);
+            return;
+        }
+	    
+        int poplar_level = 1;
+
+        int it = first;
+        int next = it + small_poplar_size;
+        while (true) {
+
+            this.unchecked_insertion_sort(array, it, next);
+
+            int poplar_size = small_poplar_size;
+
+            for (int i = (poplar_level & (0 - poplar_level)) >> 1; i != 0; i >>= 1) {
+                it -= poplar_size;
+                poplar_size = 2 * poplar_size + 1;
+                this.sift(array, it, poplar_size);
+                ++next;
+            }
+
+            if ((last - next) <= small_poplar_size) {
+                this.insertion_sort(array, next, last);
+                return;
+            }
+
+            it = next;
+            next += small_poplar_size;
+            ++poplar_level;
+        }
+    }
+
+    private void sort_heap(int[] array, int first, int last) {
+        int size = last - first;
+        if (size < 2) return;
+
+        do {
+            this.pop_heap_with_size(array, first, last, size);
+            --last;
+            --size;
+        } while (size > 1);
     }
 
     public int partition(int[] array, int a, int b, int p) {
@@ -173,7 +300,8 @@ final public class LAQuickSort extends Sort {
             if (backPivot == pivot) equalPivotCount++;
             if (depthLimit == 0 || equalPivotCount > 4){
                 if (equalPivotCount > 4) equalPivotCount = 0;
-                heapSorter.customHeapSort(arr, low, high, 1);
+                this.make_heap(arr, low, high);
+                this.sort_heap(arr, low, high);
                 return;
             }
             depthLimit--;
@@ -186,7 +314,6 @@ final public class LAQuickSort extends Sort {
 
     @Override
     public void runSort(int[] array, int currentLength, int bucketCount) {
-        this.heapSorter = new MaxHeapSort(this.arrayVisualizer);
         this.quickSort(array, 0, currentLength, 2*log2(currentLength), array[1], false, 0);
     }
 }
