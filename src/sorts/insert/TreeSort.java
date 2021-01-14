59c70bdbbd76196736dea18c2a7264ccb7b32705
aphitorite <pocketxan2@gmail.com>
2021-01-14 13:16:02 -0700

    rewrite tree sort + add some license

--- src/sorts/insert/TreeSort.java
+++ src/sorts/insert/TreeSort.java
@@ -3,13 +3,39 @@ package sorts.insert;
 import main.ArrayVisualizer;
 import sorts.templates.Sort;
 
+/*
+ * 
+MIT License
+
+Copyright (c) 2021 aphitorite
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ *
+ */
+ 
 final public class TreeSort extends Sort {
     public TreeSort(ArrayVisualizer arrayVisualizer) {
         super(arrayVisualizer);
         
         this.setSortListName("Tree");
         this.setRunAllSortsName("Unbalanced Tree Sort");
-        this.setRunSortName("Unbalanced Treesort");
+        this.setRunSortName("Treesort (Unbalanced)");
         this.setCategory("Insertion Sorts");
         this.setComparisonBased(true);
         this.setBucketSort(false);
@@ -18,78 +44,54 @@ final public class TreeSort extends Sort {
         this.setUnreasonableLimit(0);
         this.setBogoSort(false);
     }
-    
-    // Code retrieved from https://www.geeksforgeeks.org/tree-sort/
-    // clean up / refactor by aphitorite
-    
-    final private class Node { 
-        int key; 
-        Node left, right; 
-
-        public Node(int item) { 
-            key = item; 
-            left = right = null; 
-        } 
-    } 
-    
-    private Node root; 
-    private int index, length;
-
-    private Node treeWrite(Node element, int at) {
-        Node node = new Node(0);
-        
-        if(at > 0 && at < this.length) Highlights.markArray(1, at - 1);
-        Writes.changeAuxWrites(1);
-        Writes.startLap();
-        node = element;
-        Writes.stopLap();
-        
-        Delays.sleep(0.25);
-        
-        return node;
-    }
-
-    private void insert(int key) {
-        this.root = this.treeWrite(insertRec(this.root, key, 1), 1);
-    } 
-    
-    private Node insertRec(Node root, int key, int depth) { 
-        if (root == null) { 
-            root = treeWrite(new Node(key), 1);
-            Writes.changeAllocAmount(1);
-            return root; 
-        } 
-
-        if (Reads.compareValues(key, root.key) == -1) 
-            root.left  = treeWrite(insertRec(root.left, key, depth * 2), depth * 2); 
-        else
-            root.right = treeWrite(insertRec(root.right, key, (depth * 2) + 1), (depth * 2) + 1); 
-
-        return root; 
-    } 
-
-    private void traverseRec(Node root, int[] array) { 
-        if (root != null) { 
-            this.traverseRec(root.left, array); 
-            Writes.write(array, this.index++, root.key, 1, true, false); 
-            this.traverseRec(root.right, array); 
-            Writes.changeAllocAmount(-1);
-        } 
-    } 
-
-    private void treeIns(int arr[]) {
-        for(int i = 0; i < this.length; i++) {
-            Highlights.markArray(2, i);
-            this.insert(arr[i]);
-        } 
-        Highlights.clearMark(2);
-    }
+	
+	private int idx;
+	
+	private void traverse(int[] array, int[] temp, int[] lower, int[] upper, int r) {
+		Highlights.markArray(1, r);
+		Delays.sleep(1);
+		
+		if(lower[r] != 0) this.traverse(array, temp, lower, upper, lower[r]);
+		
+		Writes.write(temp, this.idx++, array[r], 0, false, true);
+		Highlights.markArray(1, r);
+		Delays.sleep(1);
+		
+		if(upper[r] != 0) this.traverse(array, temp, lower, upper, upper[r]);
+	}
 
     @Override
     public void runSort(int[] array, int currentLength, int bucketCount) {
-        this.length = currentLength;
-        this.treeIns(array); 
-        this.index = 0;
-        this.traverseRec(this.root, array); 
+		int[] lower = Writes.createExternalArray(currentLength);
+		int[] upper = Writes.createExternalArray(currentLength);
+		int[] next;
+		
+		for(int i = 1; i < currentLength; i++) {
+			Highlights.markArray(2, i);
+			int c = 0;
+			
+			while(true) {
+				Highlights.markArray(1, c);
+				Delays.sleep(0.5);
+				
+				next = Reads.compareValues(array[i], array[c]) < 0 ? lower : upper;
+				
+				if(next[c] == 0) {
+					Writes.write(next, c, i, 0, false, true);
+					break;
+				}
+				else c = next[c];
+			}
+		}
+		Highlights.clearMark(2);
+		
+		int[] temp = Writes.createExternalArray(currentLength);
+		this.idx = 0;
+		this.traverse(array, temp, lower, upper, 0);
+		Writes.arraycopy(temp, 0, array, 0, currentLength, 1, true, false);
+		
+		Writes.deleteExternalArray(lower);
+		Writes.deleteExternalArray(upper);
+		Writes.deleteExternalArray(temp);
     }
 }
