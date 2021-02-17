572c372a56d03672b5682e16075426bb8a97fd47
Gaming32 <soujournme@gmail.com>
2021-02-16 18:04:00 -0600

    fix holyGriesMills

--- src/utils/Rotations.java
+++ src/utils/Rotations.java
@@ -12,11 +12,17 @@ public class Rotations {
     // utility functions
     public static void blockSwap(int[] array, int a, int b, int len, double pause, boolean mark, boolean auxwrite) {
         for (int i = 0; i < len; i++) {
+            Writes.swap(array, b - i - 1, b + len - i - 1, pause, mark, auxwrite);
+        }
+    }
+
+    public static void swapBlocksBackwards(int[] array, int a, int b, int len, double pause, boolean mark, boolean auxwrite) {
+        for (int i = 0; i < len; i++) {
             Writes.swap(array, a + i, b + i, pause, mark, auxwrite);
         }
     }
 
-    private static void grailForwardShift(int[] array, int start, int length, double pause, boolean mark, boolean auxwrite) {
+    private static void shiftForwards(int[] array, int start, int length, double pause, boolean mark, boolean auxwrite) {
         int temp = array[start];
         for(int i = 0; i < length; i++) {
             Writes.write(array, start + i, array[start + i + 1], pause, mark, auxwrite);
@@ -24,7 +30,7 @@ public class Rotations {
         Writes.write(array, start + length, temp, pause, mark, auxwrite);
     }
 
-    private static void grailBackwardShift(int[] array, int start, int length, double pause, boolean mark, boolean auxwrite) {    
+    private static void shiftBackwards(int[] array, int start, int length, double pause, boolean mark, boolean auxwrite) {    
         int temp = array[start + length];
         for(int i = length; i > 0; i--) {
             Writes.write(array, start + i, array[start + i - 1], pause, mark, auxwrite);
@@ -80,28 +86,26 @@ public class Rotations {
     }
 
     public static void holyGriesMills(int[] array, int pos, int lenA, int lenB, double pause, boolean mark, boolean auxwrite) {
-        boolean swaps = false;
-
         while(lenA > 1 && lenB > 1) {
             while(lenA <= lenB) {
-                blockSwap(array, pos, pos + lenA, lenA, pause, mark, auxwrite);
-                pos    += lenA;
+                swapBlocksBackwards(array, pos, pos + lenA, lenA, pause, mark, auxwrite);
+                pos  += lenA;
                 lenB -= lenA;
-                swaps     = true;
             }
-            if(!swaps || (lenB > 1 && lenA > 1)) {
-                while(lenA > lenB) {
-                    blockSwap(array, pos + lenA - lenB, pos + lenA, lenB, pause, mark, auxwrite); //backward swap here
-                    lenA -= lenB;
-                }
+            
+            if(lenA <= 1 || lenB <= 1) break;
+            
+            while(lenA > lenB) {
+                blockSwap(array, pos + lenA - lenB, pos + lenA, lenB, pause, mark, auxwrite);
+                lenA -= lenB;
             }
         }
-
+        
         if(lenA == 1) {
-            grailForwardShift(array, pos, lenB, pause, mark, auxwrite);
+            shiftForwards(array, pos, lenB, pause, mark, auxwrite);
         }
         else if(lenB == 1) {
-            grailBackwardShift(array, pos, lenA, pause, mark, auxwrite);
+            shiftBackwards(array, pos, lenA, pause, mark, auxwrite);
         }
     }
 
