e0eddd354fad1c33213faca96e9e6838d6e12555
thatsOven <68780017+thatsOven@users.noreply.github.com>
2021-03-04 17:12:38 +0100

    add credits

--- src/sorts/distribute/LMSDRadixSort.java
+++ src/sorts/distribute/LMSDRadixSort.java
@@ -17,6 +17,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+thanks to Tanoshi, that came up with the idea too, and made me make a Java port of the algorithm 
 */
 
 final public class LMSDRadixSort extends Sort {
