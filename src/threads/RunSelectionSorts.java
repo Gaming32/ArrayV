b9be5961a4c1471570e4946532f2c814cb98e668
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-04-06 08:28:09 -0500

    Update RunSelectionSorts.java

--- src/threads/RunSelectionSorts.java
+++ src/threads/RunSelectionSorts.java
@@ -48,48 +48,69 @@ final public class RunSelectionSorts extends MultipleSortThread {
     private Sort QueueSort;
     private Sort DequeueSort;
     private Sort StableSelectionSort;
+    private Sort BingoSort;
+    private Sort ClassicTournamentSort;
+    private Sort ReverseSandpaperSort;
+    private Sort ReverseSelectionSort;
+    private Sort SandpaperSort;
+    private Sort StableCycleSort;
+    private Sort TriangularHeapSort;
     
     public RunSelectionSorts(ArrayVisualizer arrayVisualizer) {
         super(arrayVisualizer);
         this.sortCount = 16;
         this.categoryCount = this.sortCount;
         
-        SelectionSort       = new       SelectionSort(this.arrayVisualizer);
-        StableSelectionSort = new StableSelectionSort(this.arrayVisualizer);
-        DoubleSelectionSort = new DoubleSelectionSort(this.arrayVisualizer);
-        CycleSort           = new           CycleSort(this.arrayVisualizer);
-        MaxHeapSort         = new         MaxHeapSort(this.arrayVisualizer);
-        MinHeapSort         = new         MinHeapSort(this.arrayVisualizer);
-        FlippedMinHeapSort  = new  FlippedMinHeapSort(this.arrayVisualizer);
-        WeakHeapSort        = new        WeakHeapSort(this.arrayVisualizer);
-        TernaryHeapSort     = new     TernaryHeapSort(this.arrayVisualizer);
-        BaseNMaxHeapSort    = new    BaseNMaxHeapSort(this.arrayVisualizer);
-        SmoothSort          = new          SmoothSort(this.arrayVisualizer);
-        PoplarHeapSort      = new      PoplarHeapSort(this.arrayVisualizer);
-        TournamentSort      = new      TournamentSort(this.arrayVisualizer);
-        AsynchronousSort    = new    AsynchronousSort(this.arrayVisualizer);
-        QueueSort           = new           QueueSort(this.arrayVisualizer);
-        DequeueSort         = new         DequeueSort(this.arrayVisualizer);
+        SelectionSort         = new         SelectionSort(this.arrayVisualizer);
+        StableSelectionSort   = new   StableSelectionSort(this.arrayVisualizer);
+        DoubleSelectionSort   = new   DoubleSelectionSort(this.arrayVisualizer);
+        CycleSort             = new             CycleSort(this.arrayVisualizer);
+        MaxHeapSort           = new           MaxHeapSort(this.arrayVisualizer);
+        MinHeapSort           = new           MinHeapSort(this.arrayVisualizer);
+        FlippedMinHeapSort    = new    FlippedMinHeapSort(this.arrayVisualizer);
+        WeakHeapSort          = new          WeakHeapSort(this.arrayVisualizer);
+        TernaryHeapSort       = new       TernaryHeapSort(this.arrayVisualizer);
+        BaseNMaxHeapSort      = new      BaseNMaxHeapSort(this.arrayVisualizer);
+        SmoothSort            = new            SmoothSort(this.arrayVisualizer);
+        PoplarHeapSort        = new        PoplarHeapSort(this.arrayVisualizer);
+        TournamentSort        = new        TournamentSort(this.arrayVisualizer);
+        AsynchronousSort      = new      AsynchronousSort(this.arrayVisualizer);
+        QueueSort             = new             QueueSort(this.arrayVisualizer);
+        DequeueSort           = new           DequeueSort(this.arrayVisualizer);
+        BingoSort             = new             BingoSort(this.arrayVisualizer);
+        ClassicTournamentSort = new ClassicTournamentSort(this.arrayVisualizer);
+        ReverseSandpaperSort  = new  ReverseSandpaperSort(this.arrayVisualizer);
+        ReverseSelectionSort  = new  ReverseSelectionSort(this.arrayVisualizer);
+        SandpaperSort         = new         SandpaperSort(this.arrayVisualizer);
+        StableCycleSort       = new       StableCycleSort(this.arrayVisualizer);
+        TriangularHeapSort    = new    TriangularHeapSort(this.arrayVisualizer);
     }
 
     @Override
     protected synchronized void executeSortList(int[] array) throws Exception {
-        RunSelectionSorts.this.runIndividualSort(SelectionSort,       0, array,  128, 0.01, false);
-        RunSelectionSorts.this.runIndividualSort(StableSelectionSort, 0, array,  128, 0.5,  false);
-        RunSelectionSorts.this.runIndividualSort(DoubleSelectionSort, 0, array,  128, 0.01, false);
-        RunSelectionSorts.this.runIndividualSort(CycleSort,           0, array,  128, 0.01, false);
-        RunSelectionSorts.this.runIndividualSort(MaxHeapSort,         0, array, 2048, 1.5,  false);
-        RunSelectionSorts.this.runIndividualSort(MinHeapSort,         0, array, 2048, 1.5,  false);
-        RunSelectionSorts.this.runIndividualSort(FlippedMinHeapSort,  0, array, 2048, 1.5,  false);
-        RunSelectionSorts.this.runIndividualSort(BaseNMaxHeapSort  ,  4, array, 2048, 1.5,  false);
-        RunSelectionSorts.this.runIndividualSort(WeakHeapSort,        0, array, 2048, 1,    false);
-        RunSelectionSorts.this.runIndividualSort(TernaryHeapSort,     0, array, 2048, 1,    false);
-        RunSelectionSorts.this.runIndividualSort(SmoothSort,          0, array, 2048, 1.5,  false);
-        RunSelectionSorts.this.runIndividualSort(PoplarHeapSort,      0, array, 2048, 1,    false);
-        RunSelectionSorts.this.runIndividualSort(TournamentSort,      0, array, 2048, 1.5,  false);
-        RunSelectionSorts.this.runIndividualSort(AsynchronousSort,    0, array, 1024, 1.5,  false);
-        RunSelectionSorts.this.runIndividualSort(QueueSort,           0, array, 2048, 1,    false);
-        RunSelectionSorts.this.runIndividualSort(DequeueSort,         0, array, 2048, 1,    false);
+        RunSelectionSorts.this.runIndividualSort(SelectionSort,         0, array,  128, 0.01, false);
+        RunSelectionSorts.this.runIndividualSort(ReverseSelectionSort,  0, array,  128, 0.01, false);
+        RunSelectionSorts.this.runIndividualSort(DoubleSelectionSort,   0, array,  128, 0.01, false);
+        RunSelectionSorts.this.runIndividualSort(StableSelectionSort,   0, array,  128, 0.5,  false);
+        RunSelectionSorts.this.runIndividualSort(SandpaperSort,         0, array,  128, 0.05, false);
+        RunSelectionSorts.this.runIndividualSort(ReverseSandpaperSort,  0, array,  128, 0.05, false);
+        RunSelectionSorts.this.runIndividualSort(CycleSort,             0, array,  128, 0.01, false);
+        RunSelectionSorts.this.runIndividualSort(StableCycleSort,       0, array,  128, 0.01, false);
+        RunSelectionSorts.this.runIndividualSort(BingoSort,             0, array,  128, 0.1,  false);
+        RunSelectionSorts.this.runIndividualSort(MaxHeapSort,           0, array, 2048, 1.5,  false);
+        RunSelectionSorts.this.runIndividualSort(MinHeapSort,           0, array, 2048, 1.5,  false);
+        RunSelectionSorts.this.runIndividualSort(FlippedMinHeapSort,    0, array, 2048, 1.5,  false);
+        RunSelectionSorts.this.runIndividualSort(BaseNMaxHeapSort,      4, array, 2048, 1.5,  false);
+        RunSelectionSorts.this.runIndividualSort(TriangularHeapSort,    0, array, 2048, 1.5,  false);
+        RunSelectionSorts.this.runIndividualSort(WeakHeapSort,          0, array, 2048, 1,    false);
+        RunSelectionSorts.this.runIndividualSort(TernaryHeapSort,       0, array, 2048, 1,    false);
+        RunSelectionSorts.this.runIndividualSort(SmoothSort,            0, array, 2048, 1.5,  false);
+        RunSelectionSorts.this.runIndividualSort(PoplarHeapSort,        0, array, 2048, 1,    false);
+        RunSelectionSorts.this.runIndividualSort(TournamentSort,        0, array, 2048, 1.5,  false);
+        RunSelectionSorts.this.runIndividualSort(ClassicTournamentSort, 0, array, 2048, 1.5,  false);
+        RunSelectionSorts.this.runIndividualSort(AsynchronousSort,      0, array, 1024, 1.5,  false);
+        RunSelectionSorts.this.runIndividualSort(QueueSort,             0, array, 2048, 1,    false);
+        RunSelectionSorts.this.runIndividualSort(DequeueSort,           0, array, 2048, 1,    false);
     }
     
     @Override
