16d029539055b2f5a30316fac8c4f5b51bf82819
fungamer2-2 <56766520+fungamer2-2@users.noreply.github.com>
2021-04-17 11:38:43 -0400

    Update CocktailShellSort.java

--- src/sorts/insert/CocktailShellSort.java
+++ src/sorts/insert/CocktailShellSort.java
@@ -1,12 +1,7 @@
-/**
- * 
- */
 package sorts.insert;
 
 import main.ArrayVisualizer;
 import sorts.templates.Sort;
-
-
 
 /**
  * @author fungamer2
@@ -14,12 +9,11 @@ import sorts.templates.Sort;
  */
 public final class CocktailShellSort extends Sort {
 
-	/**
-	 * @param arrayVisualizer
-	 */
-	public CocktailShellSort(ArrayVisualizer arrayVisualizer) {
-		super(arrayVisualizer);
-		// TODO Auto-generated constructor stub
+    /**
+     * @param arrayVisualizer
+     */
+    public CocktailShellSort(ArrayVisualizer arrayVisualizer) {
+        super(arrayVisualizer);
         this.setSortListName("Cocktail Shell");
         this.setRunAllSortsName("Cocktail Shell Sort");
         this.setRunSortName("Cocktail Shellsort");
@@ -30,56 +24,54 @@ public final class CocktailShellSort extends Sort {
         this.setUnreasonablySlow(false);
         this.setUnreasonableLimit(0);
         this.setBogoSort(false);
-	}
+    }
 
-	@Override
-	public void runSort(int[] array, int sortLength, int bucketCount) throws Exception {
-		// TODO Auto-generated method stub
-	     int gap = sortLength / 2;
-	     boolean dir = true;
-	     while (gap >= 1) {
-	       if (dir) {
-	         for (int i = gap; i < sortLength; i++) {
-	           int tmp = array[i];
-	           int j = i;
-	           while (j >= gap && this.Reads.compareValues(array[j - gap], tmp) == 1) {
-	             this.Highlights.markArray(2, j - gap);
-	             this.Writes.write(array, j, array[j - gap], 0.7D, true, false);
-	             j -= gap;
-	           } 
-	           
-	           if (j - gap >= 0) {
-	             this.Highlights.markArray(2, j - gap);
-	           } else {
-	             this.Highlights.clearMark(2);
-	           } 
-	           
-	           this.Writes.write(array, j, tmp, 0.7D, true, false);
-	         } 
-	       } else {
-	         for (int i = sortLength - gap; i >= 0; i--) {
-	           int tmp = array[i];
-	           int j = i;
-	           while (j < sortLength - gap && this.Reads.compareValues(array[j + gap], tmp) == -1) {
-	             this.Highlights.markArray(2, j + gap);
-	             this.Writes.write(array, j, array[j + gap], 0.7D, true, false);
-	             j += gap;
-	           } 
-	           
-	           if (j + gap < sortLength) {
-	             this.Highlights.markArray(2, j + gap);
-	           } else {
-	             this.Highlights.clearMark(2);
-	           } 
-	           
-	           this.Writes.write(array, j, tmp, 0.7D, true, false);
-	         } 
-	       } 
-	       gap = (int)(gap / Math.sqrt(2.0D));
-	       dir = (gap == 1) ? true : (!dir);
-	     }
+    @Override
+    public void runSort(int[] array, int sortLength, int bucketCount) throws Exception {
+        int gap = sortLength / 2;
+        boolean dir = true;
+        while (gap >= 1) {
+            if (dir) {
+                for (int i = gap; i < sortLength; i++) {
+                    int tmp = array[i];
+                    int j = i;
+                    while (j >= gap && Reads.compareValues(array[j - gap], tmp) == 1) {
+                        Highlights.markArray(2, j - gap);
+                        Writes.write(array, j, array[j - gap], 0.7, true, false);
+                        j -= gap;
+                    }
 
+                    if (j - gap >= 0) {
+                        Highlights.markArray(2, j - gap);
+                    } else {
+                        Highlights.clearMark(2);
+                    }
 
-	}
+                    Writes.write(array, j, tmp, 0.7, true, false);
+                }
+            } else {
+                for (int i = sortLength - gap; i >= 0; i--) {
+                    int tmp = array[i];
+                    int j = i;
+                    while (j < sortLength - gap && this.Reads.compareValues(array[j + gap], tmp) == -1) {
+                        Highlights.markArray(2, j + gap);
+                        Writes.write(array, j, array[j + gap], 0.7, true, false);
+                        j += gap;
+                    }
+
+                    if (j + gap < sortLength) {
+                        Highlights.markArray(2, j + gap);
+                    } else {
+                        Highlights.clearMark(2);
+                    }
+
+                    Writes.write(array, j, tmp, 0.7, true, false);
+                }
+            }
+            gap /= 2;
+            dir = !dir;
+        }
+
+    }
 
 }
