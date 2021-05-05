d831193dd61a9c4f6d52885a06834c6d0cd71901
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-05-05 13:50:03 -0500

    Add Insertion Sorts to Run All

--- src/sorts/insert/GambitInsertionSort.java
+++ src/sorts/insert/GambitInsertionSort.java
@@ -32,39 +32,42 @@ public final class GambitInsertionSort extends Sort {
 		setBogoSort(false);
 
 	}
-	
+
 	private int binSearch(int[] array, int begin, int end, int target) {
-		  while (true) {
-		    int delta = end - begin;
-		    if (delta <= 0)
-		      break; 
-		    int p = begin + delta / 2;
-		    if (this.Reads.compareIndices(array, p, target, 0.5D, true) == 0) return p;
-		    
-		    if (this.Reads.compareIndices(array, p, target, 0.5D, true) > 0) { end = p; continue; }
-		     begin = p + 1;
-		  } 
-		  return end;
-	}
-	
-	private void binInsert(int[] array, int len, int start, int end) {
-		  int offset = 1;
-		  for (; offset * offset < len; offset *= 2);
-		  
-		  for (int bStart = 0, bEnd = end, i = start + offset; i < end; i++) {
-		    int target = binSearch(array, bStart, bEnd, i);
-		    
-		    int tmp = array[i];
-		    int j = i - 1;
-		    while (j >= target && array[j] > tmp) {
-		      this.Writes.write(array, j + 1, array[j], 0.125D, true, false);
-		      j--;
-		    } 
-		    array[j + 1] = tmp;
-		  } 
+		while (true) {
+			int delta = end - begin;
+			if (delta <= 0)
+				break;
+			int p = begin + delta / 2;
+			if (this.Reads.compareIndices(array, p, target, 0.5D, true) == 0)
+				return p;
+
+			if (this.Reads.compareIndices(array, p, target, 0.5D, true) > 0) {
+				end = p;
+				continue;
+			}
+			begin = p + 1;
 		}
+		return end;
+	}
 
+	private void binInsert(int[] array, int len, int start, int end) {
+		int offset = 1;
+		for (; offset * offset < len; offset *= 2)
+			;
 
+		for (int bStart = 0, bEnd = end, i = start + offset; i < end; i++) {
+			int target = binSearch(array, bStart, bEnd, i);
+
+			int tmp = array[i];
+			int j = i - 1;
+			while (j >= target && array[j] > tmp) {
+				this.Writes.write(array, j + 1, array[j], 0.125D, true, false);
+				j--;
+			}
+			array[j + 1] = tmp;
+		}
+	}
 
 	@Override
 	public void runSort(int[] array, int sortLength, int bucketCount) {
