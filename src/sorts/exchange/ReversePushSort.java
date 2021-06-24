743f43cae705624ad74da6545736ea30d444a595
Patrick Clark <patrickwclark12@gmail.com>
2021-06-24 15:07:42 -0500

    Additional Sorts: Push

---
+++ src/sorts/exchange/ReversePushSort.java
@@ -0,0 +1,55 @@
+package sorts.exchange;
+
+import main.ArrayVisualizer;
+import sorts.templates.Sort;
+
+/*
+
+PORTED TO ARRAYV BY PCBOYGAMES
+
+------------------------------
+- SORTING ALGORITHM MADHOUSE -
+------------------------------
+
+*/
+final public class ReversePushSort extends Sort {
+    public ReversePushSort(ArrayVisualizer arrayVisualizer) {
+        super(arrayVisualizer);
+        this.setSortListName("Reverse Push");
+        this.setRunAllSortsName("Reverse Push Sort");
+        this.setRunSortName("Reverse Pushsort");
+        this.setCategory("Exchange Sorts");
+        this.setComparisonBased(true);
+        this.setBucketSort(false);
+        this.setRadixSort(false);
+        this.setUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.setBogoSort(false);
+    }
+
+    @Override
+    public void runSort(int[] array, int currentLength, int bucketCount) {
+        boolean anyswaps = true;
+        int i = currentLength;
+        int gap = 1;
+        while (anyswaps) {
+            anyswaps = false;
+            i = currentLength;
+            gap = 1;
+            while (i - gap > 0) {
+                Highlights.markArray(1, (i - 1) - gap);
+                Highlights.markArray(2, i - 1);
+                Delays.sleep(0.01);
+                if (Reads.compareValues(array[(i - 1) - gap], array[i - 1]) > 0) {
+                    for (int j = 1; j <= gap; j++) {
+                        Writes.swap(array, i - 1, (i - 1) - j, 0.01, true, false);
+                    }
+                    anyswaps = true;
+                    gap++;
+                } else {
+                    i--;
+                }
+            }
+        }
+    }
+}
