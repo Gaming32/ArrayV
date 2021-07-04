3da6b741845784c4677e3e5334d9c947d52e7c82
Minh Nguyệt <hchuc2463@gmail.com>
2021-07-04 14:22:36 +0700

    Refactor and reformat again

--- src/sorts/exchange/ReverseBubbleSort.java
+++ src/sorts/exchange/ReverseBubbleSort.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package sorts.exchange;
 
 import main.ArrayVisualizer;
@@ -12,13 +9,12 @@ import sorts.templates.Sort;
  */
 public final class ReverseBubbleSort extends Sort {
 
-	/**
-	 * @param arrayVisualizer
-	 */
-	public ReverseBubbleSort(ArrayVisualizer arrayVisualizer) {
-		super(arrayVisualizer);
-		// TODO Auto-generated constructor stub
-		this.setSortListName("Reverse Bubble");
+    /**
+     * @param arrayVisualizer
+     */
+    public ReverseBubbleSort(ArrayVisualizer arrayVisualizer) {
+        super(arrayVisualizer);
+        this.setSortListName("Reverse Bubble");
         this.setRunAllSortsName("Reverse Bubble Sort");
         this.setRunSortName("Reverse Bubblesort");
         this.setCategory("Exchange Sorts");
@@ -28,23 +24,23 @@ public final class ReverseBubbleSort extends Sort {
         this.setUnreasonablySlow(false);
         this.setUnreasonableLimit(0);
         this.setBogoSort(false);
-	}
+    }
 
-	@Override
-	public void runSort(int[] array, int sortLength, int bucketCount) throws Exception {
-		// TODO Auto-generated method stub
+    @Override
+    public void runSort(int[] array, int sortLength, int bucketCount) throws Exception {
 
-		for(int i = 0; i < sortLength - 1; i++) {
-			boolean sorted = true;
-			for(int j = sortLength - 1; j > i; j--) {
-                if(Reads.compareIndices(array, j - 1, j, 0.05, true) == 1){
+        for (int i = 0; i < sortLength - 1; i++) {
+            boolean sorted = true;
+            for (int j = sortLength - 1; j > i; j--) {
+                if (Reads.compareIndices(array, j - 1, j, 0.05, true) == 1) {
                     Writes.swap(array, j - 1, j, 0.075, true, false);
                     sorted = false;
                 }
-			}
-			if(sorted) break;
-		}
+            }
+            if (sorted)
+                break;
+        }
 
-	}
+    }
 
 }
