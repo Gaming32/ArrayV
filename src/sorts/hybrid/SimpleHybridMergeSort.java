162c84c820a024082c0eb7918c5caedf3a87feb9
Minh Nguyệt <hchuc2463@gmail.com>
2021-07-29 20:25:34 +0700

    Redo Simple Hybrid Merge

--- src/sorts/hybrid/SimpleHybridMergeSort.java
+++ src/sorts/hybrid/SimpleHybridMergeSort.java
@@ -4,27 +4,15 @@ import main.ArrayVisualizer;
 import sorts.templates.Sort;
 import utils.IndexedRotations;
 
-/*
- * The Stackless Timsort was made by aphitorite.
- * This variant of Stackless Timsort has removed the galloping mode code and 
- * in the subarrays needs to be merged, used rotation if the last item of
- * the second subarray is smaller than the first item of the first subarray.
- *  
- */
-
 /**
- * @author aphitorite - the creator of Stackless Timsort
- * @author Yuri-chan2007 - who implemented this sorting algorithm from Stackless
- *         Timsort
- * @implNote This sorting algorithm is based on Stackless Timsort, which is
- *           written by aphitorite.
- * 
+ * @author Yuri-chan
+ * @author thatsOven
+ *
  */
 public final class SimpleHybridMergeSort extends Sort {
 
     public SimpleHybridMergeSort(ArrayVisualizer arrayVisualizer) {
         super(arrayVisualizer);
-
         this.setSortListName("Simple Hybrid Merge");
         this.setRunAllSortsName("Simple Hybrid Merge Sort");
         this.setRunSortName("Simple Hybrid Mergesort");
@@ -37,8 +25,10 @@ public final class SimpleHybridMergeSort extends Sort {
         this.setBogoSort(false);
     }
 
-    // The next 10 methods are refactored from Stackless Timsort (special thanks to
-    // aphitorite)
+    private void rotate(int[] array, int a, int m, int b) {
+        IndexedRotations.holyGriesMills(array, a, m, b, 1.0, true, false);
+    }
+
     private int leftBinSearch(int[] array, int a, int b, int val) {
         while (a < b) {
             int m = a + (b - a) / 2;
@@ -65,6 +55,60 @@ public final class SimpleHybridMergeSort extends Sort {
         return a;
     }
 
+    // by thatsOven
+    public int triSearch(int[] arr, int l, int h, int val, double sleep) {
+        int mid = l + ((h - l) / 2);
+        Highlights.markArray(0, l);
+        Highlights.markArray(1, h);
+        Highlights.markArray(2, mid);
+        Delays.sleep(sleep);
+        if (Reads.compareValues(val, arr[l]) < 0) {
+            return l;
+        } else {
+            if (Reads.compareValues(val, arr[h]) < 0) {
+                if (Reads.compareValues(val, arr[mid]) < 0) {
+                    return this.triSearch(arr, l + 1, mid - 1, val, sleep);
+                } else {
+                    return this.triSearch(arr, mid + 1, h - 1, val, sleep);
+                }
+            } else {
+                return h + 1;
+            }
+        }
+    }
+
+    private void standardInsert(int[] array, int start, int end, double compSleep, double writeSleep) {
+        int i = start + 1;
+        if (Reads.compareIndices(array, i - 1, i++, compSleep, true) == 1) {
+            while (i < end && Reads.compareIndices(array, i - 1, i, compSleep, true) == 1)
+                i++;
+            Writes.reversal(array, start, i - 1, writeSleep, true, false);
+        } else
+            while (i < end && Reads.compareIndices(array, i - 1, i, compSleep, true) <= 0)
+                i++;
+
+        Highlights.clearMark(2);
+
+        while (i < end) {
+            int num = array[i];
+            int lo = start;
+
+            lo = this.triSearch(array, start, i - 1, num, compSleep);
+            Highlights.clearAllMarks();
+
+            int j = i - 1;
+
+            while (j >= lo) {
+                Writes.write(array, j + 1, array[j], writeSleep, true, false);
+                j--;
+            }
+            Writes.write(array, lo, num, writeSleep, true, false);
+
+            Highlights.clearAllMarks();
+            i++;
+        }
+    }
+
     private int leftBoundSearch(int[] array, int a, int b, int val) {
         int i = 1;
         while (a - 1 + i < b && Reads.compareValues(val, array[a - 1 + i]) >= 0)
@@ -79,47 +123,6 @@ public final class SimpleHybridMergeSort extends Sort {
             i *= 2;
 
         return this.leftBinSearch(array, Math.max(a, b - i + 1), b - i / 2, val);
-    }
-
-    private void insertTo(int[] array, int a, int b) {
-        Highlights.clearMark(2);
-
-        if (a > b) {
-            int temp = array[a];
-
-            do
-                Writes.write(array, a, array[--a], 0.25, true, false);
-            while (a > b);
-
-            Writes.write(array, b, temp, 0.25, true, false);
-        }
-    }
-
-    private void rotate(int[] array, int a, int m, int b) {
-        IndexedRotations.holyGriesMills(array, a, m, b, 1.0, true, false);
-    }
-
-    private void buildRuns(int[] array, int a, int b, int mRun) {
-        int i = a + 1, j = a;
-
-        while (i < b) {
-            if (Reads.compareIndices(array, i - 1, i++, 1, true) == 1) {
-                while (i < b && Reads.compareIndices(array, i - 1, i, 1, true) == 1)
-                    i++;
-                Writes.reversal(array, j, i - 1, 1, true, false);
-            } else
-                while (i < b && Reads.compareIndices(array, i - 1, i, 1, true) <= 0)
-                    i++;
-
-            if (i < b)
-                j = i - (i - j - 1) % mRun - 1;
-
-            while (i - j < mRun && i < b) {
-                this.insertTo(array, i, this.rightBinSearch(array, j, i, array[i]));
-                i++;
-            }
-            j = i++;
-        }
     }
 
     protected void mergeFW(int[] array, int[] temp, int start, int mid, int end) {
@@ -181,30 +184,30 @@ public final class SimpleHybridMergeSort extends Sort {
             this.mergeBW(array, tmp, a, m, b);
         else
             this.mergeFW(array, tmp, a, m, b);
-
     }
 
-    public void adaptiveMergeSort(int[] array, int start, int end) {
-        int[] tmp = Writes.createExternalArray((end - start) / 2);
-        int mRun = end - start;
-        for (; mRun >= 32; mRun = (mRun + 1) / 2)
-            ;
-
-        this.buildRuns(array, start, end, mRun);
-
-        for (int i, j = mRun; j < (end - start); j *= 2) {
-            for (i = start; i + 2 * j <= end; i += 2 * j)
-                this.smartMerge(array, tmp, i, i + j, i + 2 * j);
-
-            if (i + j < end)
-                this.smartMerge(array, tmp, i, i + j, end);
+    protected void mergeSort(int[] array, int[] tmp, int start, int end) {
+        if (end - start > 16) {
+            int mid = start + (end - start) / 2;
+            mergeSort(array, tmp, start, mid);
+            mergeSort(array, tmp, mid, end);
+            smartMerge(array, tmp, start, mid, end);
+        } else {
+            standardInsert(array, start, end, 0.25, 0.5);
         }
+    }
+
+    public void customSort(int[] array, int start, int end) {
+        int[] tmp = Writes.createExternalArray((end - start) / 2);
+        mergeSort(array, tmp, start, end);
         Writes.deleteExternalArray(tmp);
     }
 
     @Override
     public void runSort(int[] array, int sortLength, int bucketCount) {
-        adaptiveMergeSort(array, 0, sortLength);
+        int[] tmp = Writes.createExternalArray(sortLength / 2);
+        mergeSort(array, tmp, 0, sortLength);
+        Writes.deleteExternalArray(tmp);
 
     }
 
