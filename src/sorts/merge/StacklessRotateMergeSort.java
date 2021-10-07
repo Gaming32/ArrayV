d770ecba1698a1d4cfdb8e8d2f7ca12aad929c46
aphitorite <66573194+aphitorite@users.noreply.github.com>
2021-10-06 23:21:53 -0600

    add stackless rotate merge for reals

--- src/sorts/merge/StacklessRotateMergeSort.java
+++ src/sorts/merge/StacklessRotateMergeSort.java
@@ -2,12 +2,13 @@ package sorts.merge;
 
 import main.ArrayVisualizer;
 import sorts.templates.Sort;
+import utils.IndexedRotations;
 
 /*
  * 
 The MIT License (MIT)
 
-Copyright (c) 2020 aphitorite
+Copyright (c) 2021 aphitorite
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -44,135 +45,82 @@ final public class StacklessRotateMergeSort extends Sort {
         this.setBogoSort(false);
     }
 	
-	private int val;
-	
-	private void valSwap(int[] array, int i) {
-		int t = array[i];
-		Writes.write(array, i, this.val, 1, true, false);
-		this.val = t;
-	}
-	
-    private void multiSwap(int[] array, int a, int b, int len) {
-		for(int i = 0; i < len; i++)
-			Writes.swap(array, a+i, b+i, 1, true, false);
-	}
-	
     private void rotate(int[] array, int a, int m, int b) {
-        int l = m-a, r = b-m;
-		
-        while(l > 0 && r > 0) {
-			if(r < l) {
-				this.multiSwap(array, m-r, m, r);
-				b -= r;
-				m -= r;
-				l -= r;
-            }
-            else {
-				this.multiSwap(array, a, m, l);
-				a += l;
-				m += l;
-				r -= l;
-            }
-        }
+		IndexedRotations.griesMills(array, a, m, b, 1, true, false);
     }
 	
-	private int binSearch(int[] array, int a, int b, int value, boolean left) {
-		while(a < b) {
-			int m = a+(b-a)/2;
+	//@param c - select c smallest elements
+	private void partitionMerge(int[] array, int a, int m, int b, int c) {
+		int lenA = m-a, lenB = b-m;
+		
+		if(lenA < 1 || lenB < 1) return;
+		
+		if(lenB < lenA) {
+			c = (lenA+lenB)-c;
+			int r1 = 0, r2 = Math.min(c, lenB);
 			
-			boolean comp = left ? Reads.compareValues(value, array[m]) <= 0
-								: Reads.compareValues(value, array[m]) < 0;
-			
-			if(comp) b = m;
-			else     a = m+1;
+			while(r1 < r2) {
+				int ml = (r1+r2)/2;
+				
+				if(Reads.compareValues(array[m-(c-ml)], array[b-ml-1]) > 0)
+					r2 = ml;
+				else
+					r1 = ml+1;
+			}
+			//[lenA-(c-r1)][c-r1][lenB-r1][r1]
+			//[lenA-(c-r1)][lenB-r1][c-r1][r1]
+			this.rotate(array, m-(c-r1), m, b-r1);
 		}
-		
-		return a;
-	}
-    
-    private void rotateMerge(int[] array, int a, int m, int b) {
-		int p = m, q = b;
-		
-		while(a < b) {
-			while(p-a > 0 && q-p > 0) {
-				int m1, m2, m3;
-				
-				if(p-a >= q-p) {
-					m1 = a+(p-a)/2;
-					m2 = this.binSearch(array, p, q, array[m1], true);
-					m3 = m1+(m2-p);
-				}
-				else {
-					m2 = p+(q-p)/2;
-					m1 = this.binSearch(array, a, p, array[m2], false);
-					m3 = (m2++)-(p-m1);
-				}
-				this.rotate(array, m1, p, m2);
-				this.valSwap(array, m3);
-				
-				p = m1;
-				q = m3;
-			}
-			Highlights.clearMark(2);
+		else {
+			int r1 = 0, r2 = Math.min(c, lenA);
 			
-			this.valSwap(array, q);
-			a = q+1;
-			for(q = a; q < b && Reads.compareValues(this.val, array[q]) > 0; q++) {
-				Highlights.markArray(1, q);
-				Delays.sleep(0.5);
+			while(r1 < r2) {
+				int ml = (r1+r2)/2;
+				
+				if(Reads.compareValues(array[a+ml], array[m+(c-ml)-1]) > 0)
+					r2 = ml;
+				else
+					r1 = ml+1;
 			}
-			for(p = a+1; p < q && Reads.compareIndices(array, p-1, p, 0.5, true) <= 0; p++);
+			//[r1][lenA-r1][c-r1][lenB-(c-r1)]
+			//[r1][c-r1][lenA-r1][lenB-(c-r1)]
+			this.rotate(array, a+r1, m, m+(c-r1));
 		}
 	}
 	
-	protected void rotateMergeSort(int[] array, int a, int b) {
-		this.val = b; //MAX_VALUE
-        int len = b-a, i;
+	private void rotateMerge(int[] array, int a, int b, int c) {
+		int i;
+		for(i = a+1; i < b && Reads.compareIndices(array, i-1, i, 0.25, true) <= 0; i++);
+		if(i < b) this.partitionMerge(array, a, i, b, c);
+	}
+	
+	public void rotatePartitionMergeSort(int[] array, int a, int b) {
+        int len = b-a;
         
-        for(int j = 1; j < len; j *= 2) {
-            for(i = a; i + 2*j <= b; i += 2*j)
-                this.rotateMerge(array, i, i+j, i+2*j);
-            
-            if(i + j < b)
-                this.rotateMerge(array, i, i+j, b);
+		for(int i = a+1; i < b; i += 2)
+			if(Reads.compareIndices(array, i-1, i, 0.5, true) > 0)
+				Writes.swap(array, i-1, i, 0.5, true, false);
+				
+        for(int j = 2; j < len; j *= 2) {
+			int b1 = 0;
+			
+            for(int i = a; i+j < b; i += 2*j) {
+				b1 = Math.min(i+2*j, b);
+                this.partitionMerge(array, i, i+j, b1, j);
+			}
+			
+			for(int k = j/2; k > 1; k /= 2)
+				for(int i = a; i+k < b1; i += 2*k)
+					this.rotateMerge(array, i, Math.min(i+2*k, b), k);
+				
+			for(int i = a+1; i < b1; i += 2)
+				if(Reads.compareIndices(array, i-1, i, 0.5, true) > 0)
+					Writes.swap(array, i-1, i, 0.5, true, false);
         }
     }
     
-	public void sort(int[] array, int start, int end, double sleep) {
-		int min = array[start], max = min;
-		for(int i = start+1; i < end; i++) {
-            if(array[i] < min) min = array[i];
-            else if(array[i] > max) max = array[i];
-        }
-        
-        int size = max - min + 1;
-        int[] holes = new int[size];
-        
-        for(int i = start; i < end; i++)
-            Writes.write(holes, array[i] - min, holes[array[i] - min] + 1, 0, false, true);
-        
-        for(int i = 0, j = start; i < size; i++) {
-            while(holes[i] > 0) {
-                Writes.write(holes, i, holes[i] - 1, 0, false, true);
-                Writes.write(array, j, i + min, sleep, true, false);
-                j++;
-            }
-        }
-	}
-	
-	private void test(int[] array, int a, int b) {
-		int m = (a+b)/2;
-		sort(array, a, m, 1);
-		sort(array, m, b, 1);
-		this.rotateMerge(array, a, m, b);
-	}
-	
     @Override
     public void runSort(int[] array, int length, int bucketCount) {
-		this.val = length; //MAX_VALUE
-		this.test(array, 0, length/2);
-		this.test(array, length/2, length);
-		this.rotateMerge(array, 0, length/2, length);
-		//this.rotateMergeSort(array, 0, length);
+		this.rotatePartitionMergeSort(array, 0, length);
     }
 }
