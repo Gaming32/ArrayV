5664c9517d04d7cdc6814a4d8f2bae22164bf089
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-03-02 16:01:09 -0600

    Document ArrayVScript

--- src/main/java/io/github/arrayv/groovyapi/ArrayVScript.java
+++ src/main/java/io/github/arrayv/groovyapi/ArrayVScript.java
@@ -4,7 +4,15 @@ import groovy.lang.MissingPropertyException;
 import groovy.lang.Script;
 import io.github.arrayv.sortdata.SortInfo;
 
+/**
+ * This is the base class for all Groovy scripts in ArrayV. It is primarily used for accessing sorts
+ * directly using their internal name, instead of having to use {@link GroovyLocals#getSort(String)}.
+ * This class may be extended in the future.
+ */
 public abstract class ArrayVScript extends Script {
+    /**
+     * Accesses sorts by internal name when the specified property isn't found.
+     */
     @Override
     public Object getProperty(String property) {
         try {
