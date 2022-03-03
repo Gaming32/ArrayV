808ff42660496dfb293a1bced3bb2a7eee2af729
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-03-03 12:07:48 -0600

    Document RunSortInfoExtension

--- src/main/java/io/github/arrayv/groovyapi/exts/RunSortInfoExtension.java
+++ src/main/java/io/github/arrayv/groovyapi/exts/RunSortInfoExtension.java
@@ -3,19 +3,45 @@ package io.github.arrayv.groovyapi.exts;
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Map;
 
+import io.github.arrayv.groovyapi.RunSortBuilder;
+
+/**
+ * Extension for creating {@link Map.Entry}s to configure {@link RunSortBuilder} with
+ * <br>
+ * See {@link RunSortBuilder} for more info on these keys
+ */
 public final class RunSortInfoExtension {
     private RunSortInfoExtension() {
     }
 
+    /**
+     * Create a {@code numbers} key
+     * @param self The number of numbers
+     * @return A {@link Map.Entry} for {@code numbers}
+     */
     public static Map.Entry<String, Object> getNumbers(Integer self) {
-        return new SimpleImmutableEntry<>("numbers", self);
+        return create("numbers", self);
     }
 
+    /**
+     * Create a {@code buckets} key
+     * @param self The extra value to pass to the sort
+     * @return A {@link Map.Entry} for {@code buckets}
+     */
     public static Map.Entry<String, Object> getBuckets(Integer self) {
-        return new SimpleImmutableEntry<>("buckets", self);
+        return create("buckets", self);
     }
 
+    /**
+     * Create a {@code delay} key
+     * @param self The speed multiplier for the visual
+     * @return A {@link Map.Entry} for {@code delay}
+     */
     public static Map.Entry<String, Object> getDelay(Number self) {
-        return new SimpleImmutableEntry<>("delay", self.doubleValue());
+        return create("delay", self);
+    }
+
+    private static Map.Entry<String, Object> create(String key, Object self) {
+        return new SimpleImmutableEntry<>(key, self);
     }
 }
