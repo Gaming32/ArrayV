4edfc9184731e47eabf3b465c8ee44fe0dfa40e2
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-03-03 12:03:23 -0600

    Document RunSortBuilder

--- src/main/java/io/github/arrayv/groovyapi/RunSortBuilder.java
+++ src/main/java/io/github/arrayv/groovyapi/RunSortBuilder.java
@@ -11,6 +11,7 @@ import javax.swing.JOptionPane;
 
 import io.github.arrayv.frames.ArrayFrame;
 import io.github.arrayv.groovyapi.ScriptManager.ScriptThread;
+import io.github.arrayv.groovyapi.exts.RunSortInfoExtension;
 import io.github.arrayv.main.ArrayManager;
 import io.github.arrayv.main.ArrayVisualizer;
 import io.github.arrayv.panes.JErrorPane;
@@ -19,6 +20,23 @@ import io.github.arrayv.utils.Delays;
 import io.github.arrayv.utils.StopSort;
 import io.github.arrayv.utils.Timer;
 
+/**
+ * <p>This class is used for running sorting algorithms. You can construct an instance using the
+ * {@link GroovyLocals#run(SortInfo)} method, and other {@code run} methods.</p>
+ *
+ * <p>This class is configured using a map of options. This map can contain a strict set of keys
+ * Here is the list of allowed keys and their types:</p>
+ * <ul>
+ *   <li>{@code numbers} ({@code Integer}) &mdash; The number of values to sort</li>
+ *   <li>{@code buckets} ({@code Integer}) &mdash; An extra value to pass to the sorting
+ *       algorithm</li>
+ *   <li>{@code delay} ({@code Number}) &mdash; The time multiplier of the visual</li>
+ * </ul>
+ * <p>{@link Map.Entry} values to put into the option map can be obtained using {@link RunSortInfoExtension}.</p>
+ *
+ * @see GroovyLocals#run(SortInfo)
+ * @see RunSortInfoExtension
+ */
 public final class RunSortBuilder {
     private static final Set<String> ALLOWED_KEYS = new HashSet<>(Arrays.asList(
         "numbers",
@@ -61,10 +79,19 @@ public final class RunSortBuilder {
         }
     }
 
+    /**
+     * The sorting algorithm this builder will run
+     * @return The sorting algorithm this builder will run
+     * @see SortInfo
+     */
     public SortInfo getSort() {
         return sort;
     }
 
+    /**
+     * The options configured for this builder
+     * @return The options configured for this builder
+     */
     public Map<String, Object> getOpts() {
         return unmodifiableOpts == null ? (unmodifiableOpts = Collections.unmodifiableMap(opts)) : unmodifiableOpts;
     }
@@ -74,6 +101,12 @@ public final class RunSortBuilder {
         return "RunSortBuilder[sort=" + sort + ", opts=" + opts + "]";
     }
 
+    /**
+     * Merge the specified options with the options map
+     * @param opts The options to merge, generally obtained with {@link RunSortInfoExtension}
+     * @return {@code this} for chaining
+     * @see RunSortInfoExtension
+     */
     @SafeVarargs
     public final RunSortBuilder with(Map.Entry<String, Object>... opts) {
         for (Map.Entry<String, Object> opt : opts) {
@@ -89,25 +122,48 @@ public final class RunSortBuilder {
         opts.put(opt.getKey(), opt.getValue());
     }
 
+    /**
+     * The configured length of the array (key of {@code "numbers"})
+     * @return The configured length
+     * @see RunSortInfoExtension#getNumbers
+     */
     public int getLength() {
         // @checkstyle:off LeftCurlyCheck - It's more readable this way, I think
         return ((Number)opts.computeIfAbsent("numbers", k -> { throw new NullPointerException("numbers"); })).intValue();
         // @checkstyle:on LeftCurlyCheck
     }
 
+    /**
+     * The extra value to pass to the sort (key of {@code "buckets"})
+     * @return The extra value
+     * @see RunSortInfoExtension#getBuckets
+     */
     public int getBuckets() {
         return ((Number)opts.getOrDefault("buckets", 0)).intValue();
     }
 
+    /**
+     * The speed multiplier of the visual (key of {@code "delay"})
+     * @return The speed multiplier
+     * @see RunSortInfoExtension#getDelay
+     */
     public double getDelay() {
         return ((Number)opts.getOrDefault("delay", 1.0)).doubleValue();
     }
 
+    /**
+     * <p>Put one more option into the options map and run the sort</p>
+     * Equivalent to {@code with(opt).go()} or {@code with opt go()}
+     * @param opt The option to add
+     */
     public void and(Map.Entry<String, Object> opt) {
         put(opt);
         finish();
     }
 
+    /**
+     * Run the sort
+     */
     public void go() {
         finish();
     }
