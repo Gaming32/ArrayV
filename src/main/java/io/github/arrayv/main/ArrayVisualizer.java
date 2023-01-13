01ec82b92cd4a022f163950a73f7c375ff92bbe3
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-01-13 07:06:15 -0600

    Change import sort message

--- src/main/java/io/github/arrayv/main/ArrayVisualizer.java
+++ src/main/java/io/github/arrayv/main/ArrayVisualizer.java
@@ -293,8 +293,15 @@ public final class ArrayVisualizer {
                     }
                     ArrayVisualizer.this.sortAnalyzer.sortSorts();
                     ArrayVisualizer.this.refreshSorts();
-                    String message = "Successfully imported " + success + "/" + droppedFiles.size() + " sorts";
-                    JOptionPane.showMessageDialog(null, message, "Import Sorts", JOptionPane.INFORMATION_MESSAGE);
+                    if (success == 0) {
+                        JErrorPane.invokeCustomErrorMessage("Failed to import all " + droppedFiles.size() + " sorts");
+                    } else {
+                        String message = "Successfully imported " + success + " sorts";
+                        if (success < droppedFiles.size()) {
+                            message += " and failed to import " + (droppedFiles.size() - success);
+                        }
+                        JOptionPane.showMessageDialog(null, message, "Import Sorts", JOptionPane.INFORMATION_MESSAGE);
+                    }
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
